bced85e0c2964eeed1804349d4d9d01ebc7c5e38
Maks <maks.vogtman@gmail.com>
2026-05-26 18:17:27 +0200

    Initial Commit

---
+++ UrbanStrideFlow/UrbanStrideFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanStrideFlowApp.swift
+//  UrbanStrideFlow
+//
+//  Created by Maks Vogtman on 26/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanStrideFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
